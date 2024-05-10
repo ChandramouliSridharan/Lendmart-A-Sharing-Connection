e5d68363337e75cf538eee05a1aac232e7aaca77
ChandramouliSridharan <76207314+ChandramouliSridharan@users.noreply.github.com>
2024-05-10 17:32:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCctCeiw0OSeXm9hxRoEAGo9jCdwadQuH8",
-  authDomain: "lendmart-90895.firebaseapp.com",
-  projectId: "lendmart-90895",
-  storageBucket: "lendmart-90895.appspot.com",
-  messagingSenderId: "49647665043",
-  appId: "1:49647665043:web:eb6a98b512e51c042ff266",
-  measurementId: "G-JJG3DG3BCD"
+    // Give your firebase Config key here..
 };
 
 if(!firebase.apps.length){
